ccb04a3a7be3ce93be76e85e67ef178d33386ab7
farrahalyssa <idcfarrah@gmail.com>
2024-11-17 14:20:53 -0500

    updating dependencies

--- client/src/utils/api.tsx
+++ client/src/utils/api.tsx
@@ -1,7 +1,10 @@
 import axios from 'axios';
+import dotenv from 'dotenv';
 
+dotenv.config();
 const api = axios.create({
-    baseURL: 'http://localhost:8080/api',
+    baseURL: `${process.env.API_URL}/api`,
+    // 'http://localhost:8080/api',
     headers: {
       'Content-Type': 'application/json',
     },
