ea51a0735cd912103161ca8e24ddb0cd3d16e5b4
farrahalyssa <idcfarrah@gmail.com>
2024-11-17 16:12:27 -0500

    update api

--- client/src/utils/api.tsx
+++ client/src/utils/api.tsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://tala-app-server.vercel.app/api',
+  baseURL: 'http://tala-app-server.vercel.app/api',
   headers: {
     'Content-Type': 'application/json',
   },
