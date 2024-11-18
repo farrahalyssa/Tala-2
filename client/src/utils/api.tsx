f660f49a93e67ac50ce2ccac81fbd97ccb359a7e
farrahalyssa <idcfarrah@gmail.com>
2024-11-18 01:32:48 -0500

    update urls

--- client/src/utils/api.tsx
+++ client/src/utils/api.tsx
@@ -1,18 +1,20 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://tala-2.onrender.com/api',
+    baseURL: 'http://localhost:5001/api' || 'https://tala-2.onrender.com/api',
     headers: {
       'Content-Type': 'application/json',
     },
   });
 
-  api.interceptors.request.use((config) => {
-    const token = localStorage.getItem('token');
-    if (token) {
-      config.headers['x-auth-token'] = token;
-    }
-    return config;
-  });
   
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem('token'); // Retrieve token from localStorage
+  if (token) {
+      config.headers['Authorization'] = `Bearer ${token}`; // Set token in Authorization header
+  }
+  return config;
+}, (error) => {
+  return Promise.reject(error);
+});
   export default api;
