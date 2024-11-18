a930db79b976140cc8c932d1cbf3bad4ea6bb9ce
farrahalyssa <idcfarrah@gmail.com>
2024-11-17 21:24:13 -0500

    Revert "merge branch"
    This reverts commit 8990eaadfbd21806a67822ef6bb91476af99095c.

--- server/controllers/User/getUserData.js
+++ server/controllers/User/getUserData.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 const mongoose = require('mongoose'); 
 const { User } = require('../../models/userModel');
 
@@ -10,13 +11,16 @@ const getUserData = async (req, res) => {
     return res.status(400).send({ message: 'No userId provided.' });
   }
 
+  // Validate userId as a MongoDB ObjectId
   if (!mongoose.Types.ObjectId.isValid(userId)) {
     return res.status(400).send({ message: 'Invalid userId format.' });
   }
 
   try {
+    // Convert userId to ObjectId
     const userObjectId = new mongoose.Types.ObjectId(userId);
 
+    // Query the database
     const userData = await User.findOne({ _id: userObjectId }).lean();
 
     if (!userData) {
@@ -28,6 +32,23 @@ const getUserData = async (req, res) => {
     console.error('Error fetching user data:', error);
     res.status(500).send({ message: 'Internal Server Error' });
   }
+=======
+const {User} = require('../../models/userModel')
+
+const getUserData = async (req, res) => {
+    const { userId } = req.params;
+    if (!userId) {
+        return res.status(400).send({ message: 'No userId fetched.' });
+    }
+
+    try {
+        const userData = User.findOne({ userId: userId });
+        res.status(200).send(userData);
+    } catch (error) {
+        console.error('Error fetching user data:', error);
+        res.status(500).send({ message: 'Internal Server Error' });
+    }
+>>>>>>> f6d6bce4d1d78d3a43491a153a8f1b5c71de9659
 };
 
 module.exports = getUserData;
