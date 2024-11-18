bc48febbcfc77ca1f832783c51b730b52cd47c6b
farrahalyssa <idcfarrah@gmail.com>
2024-11-17 20:44:08 -0500

    fixing dependencies

--- server/index.js
+++ server/index.js
@@ -5,6 +5,14 @@ require('./models/postModel');
 const express = require('express')
 const app = express()
 const cors = require('cors')
+
+const corsOptions = {
+    origin: 'https://tala-app.netlify.app', 
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Add other methods if needed
+    credentials: true, // Allow credentials (e.g., cookies, authorization headers)
+  };
+  
+  app.use(cors(corsOptions));
 const connection = require('./db')
 const authRoutes = require('./routes/authRoutes')
 const postRoutes = require('./routes/postRoutes')
