8def9f7109aa7cacce62dfa55ee3e33c6b3225a7
farrahalyssa <idcfarrah@gmail.com>
2024-11-18 12:59:03 -0500

    update access control

--- server/accessControlAllow.js
+++ server/accessControlAllow.js
@@ -1,15 +1,18 @@
 
 
-const FRONTEND_URL = 'https://tala-app.netlify.app' || 'http://localhost:5173';
+const allowedOrigins = [
+  "https://tala-app.netlify.app",
+  "http://localhost:5173"
+];
 
-if (!FRONTEND_URL) {
-  throw new Error("FRONTEND_URL value is not defined in .env file");
-}
 
 const accessControlAllow = (req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", 'https://tala-app.netlify.app/');
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes(origin)) {
+    res.setHeader("Access-Control-Allow-Origin", origin);
+  }
   res.setHeader("Access-Control-Allow-Credentials", "true");
-  res.setHeader("Access-Control-Allow-Methods", "POST,GET,OPTIONS");
+  res.setHeader("Access-Control-Allow-Methods", "POST,GET, DELETE, PUT, OPTIONS");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
