b36c17a53e8aef7db887a0c1cceaaba19b5e4790
farrahalyssa <idcfarrah@gmail.com>
2024-11-18 13:58:16 -0500

    update index.js

--- server/index.js
+++ server/index.js
@@ -36,6 +36,10 @@ app.use('/api/post', postRoutes);
 app.use('/api/users', userRoutes);
 
 // Server and Serverless Configuration
+
+app.post('/api/auth/login', (req, res) => {
+    res.json({ message: 'CORS configured correctly!' });
+  });
 const port = process.env.PORT || 5003;
 if (process.env.NODE_ENV !== 'serverless') {
   app.listen(port, () => console.log(`Server running on port ${port}`));
