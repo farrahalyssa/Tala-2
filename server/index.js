e2aa49684626e51d2af1e8b6251ffad2aeef0ef2
farrahalyssa <idcfarrah@gmail.com>
2024-11-27 14:03:07 -0500

    update index cors origin

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 const express = require('express');
 
-const cors = require('cors', { origin: 'http://localhost:5173' });
+const cors = require('cors', { origin: '*' });
 
 const connection = require('./db');
 const authRoutes = require('./routes/authRoutes');
