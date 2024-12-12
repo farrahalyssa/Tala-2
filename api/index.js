130cf52952d5aa877bf4b8bf432b33f67038e81b
farrahalyssa <idcfarrah@gmail.com>
2024-12-12 13:22:17 -0500

    fix index

--- api/index.js
+++ api/index.js
@@ -4,45 +4,18 @@ const express = require('express');
 
 const cors = require('cors');
 
-const connection = require('./api/db');
-const authRoutes = require('./api/routes/authRoutes');
-const postRoutes = require('./api/routes/postRoutes');
-const userRoutes = require('./api/routes/userRoutes');
+const connection = require('./db');
+const authRoutes = require('./routes/authRoutes');
+const postRoutes = require('./routes/postRoutes');
+const userRoutes = require('./routes/userRoutes');
 
 const app = express();
 
-app.use(express.static('../client/dist'));
-app.get('*', (req, res) => {
-    res.sendFile(path.join('../client/dist', 'index.html'));
-  });
 
 connection();
 
 app.use(express.json());
 
-const allowCors = fn => async (req, res) => {
-  res.setHeader('Access-Control-Allow-Credentials', true)
-  res.setHeader('Access-Control-Allow-Origin', '*')
-  // another common pattern
-  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
-  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
-  res.setHeader(
-    'Access-Control-Allow-Headers',
-    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-  )
-  if (req.method === 'OPTIONS') {
-    res.status(200).end()
-    return
-  }
-  return await fn(req, res)
-}
-
-const handler = (req, res) => {
-  const d = new Date()
-  res.end(d.toString())
-}
-
-module.exports = allowCors(handler)
 
  app.use(cors());
 
